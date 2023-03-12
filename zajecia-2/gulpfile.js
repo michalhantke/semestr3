b467d440b54f1ae7d02a318116da62f022e0029b
michalhantke <michal.hantke@shoper.pl>
2023-03-12 21:24:17 +0100

    paczka gulp-imagemin i zadanie minifikacji obrazków

---
+++ zajecia-2/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+
+const css = function() {
+    return gulp.src("./src/scss/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./dist/css"))
+};
+
+const minification = function() {
+    return gulp.src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"))
+};
+
+exports.default = css;
+exports.minification = minification;
